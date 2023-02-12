e61713afd456657f02d03c9444455c2f93885c8c
Sergiu Corbu <sergiu.corbu@tapptitude.com>
2023-02-12 16:06:28 +0200

    initial project setup

---
+++ Advanced-SwiftUI/Advanced-SwiftUI/Advanced_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Advanced_SwiftUIApp.swift
+//  Advanced-SwiftUI
+//
+//  Created by Sergiu Corbu on 12.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct Advanced_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
